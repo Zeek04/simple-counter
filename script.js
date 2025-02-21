df718ff1b6ed4627713a9d353ad7b83d0f18d663
Ezequiel <cruzezequiel81@gmail.com>
2025-02-21 01:06:40 -0500

    Counter will now add and subtract 5 when clicked

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const counter = document.getElementById('score');
 const plus = document.getElementById("plus");
 const minus = document.getElementById('minus');
+const addFive = document.getElementById('plus5')
+const minusFive = document.getElementById('minus5')
 let score = 0;
 
 plus.addEventListener('click', () => {
@@ -9,9 +11,21 @@ plus.addEventListener('click', () => {
     counter.textContent = score
 })
 
+addFive.addEventListener('click', () => {
+    score = score + 5
+    counter.textContent = score
+})
+
 minus.addEventListener('click', () => {
     if(score >= 1){
         score--
+        counter.textContent = score
+    }
+})
+
+minusFive.addEventListener('click', () => {
+    if(score >= 5){
+        score = score - 5
         counter.textContent = score
     }
 })
